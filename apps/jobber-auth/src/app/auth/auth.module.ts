379d59afe67863eff5cfacab1f5b9c474b67a7f0
Nazim <yasarnazimsari@gmail.com>
2025-03-10 22:26:35 +0300

    created jwt sign cookie

--- apps/jobber-auth/src/app/auth/auth.module.ts
+++ apps/jobber-auth/src/app/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthResolver } from './auth.resolver';
 import { AuthService } from './auth.service';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService, ConfigModule } from '@nestjs/config';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    ConfigModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
@@ -16,6 +18,7 @@ import { ConfigService, ConfigModule } from '@nestjs/config';
       }),
       inject: [ConfigService],
     }),
+    UsersModule,
   ],
   providers: [AuthResolver, AuthService],
 })
