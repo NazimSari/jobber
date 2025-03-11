ef263cfba88a731e658375f9e0744964a1569117
Nazim <yasarnazimsari@gmail.com>
2025-03-11 17:25:29 +0300

    updated eslint.config, tsconfig app.json, tsconfig.json webpack.config.js, main.ts, app.module.ts

--- apps/jobber-jobs/webpack.config.js
+++ apps/jobber-jobs/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/jobber-jobs'),
+    path: join(__dirname, '../../dist/apps/jobber-jobs'),
   },
   plugins: [
     new NxAppWebpackPlugin({
