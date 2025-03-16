b53cda351583b050b68f57c636a5260139e71b04
Nazim <yasarnazimsari@gmail.com>
2025-03-16 16:05:07 +0300

    updated pulsar.client and created pulsar.consumer file

---
+++ libs/pulsar/src/lib/pulsar.consumer.ts
@@ -0,0 +1,23 @@
+import { Consumer, Message } from 'pulsar-client';
+import { PulsarClient } from './pulsar.client';
+
+export abstract class PulsarConsumer {
+  private consumer!: Consumer;
+
+  constructor(
+    private readonly pulsarClient: PulsarClient,
+    private readonly topic: string
+  ) {}
+
+  async onModuleInit() {
+    this.consumer = await this.pulsarClient.createConsumer(
+      this.topic,
+      this.onMessage.bind(this)
+    );
+  }
+  protected async ackknowledge(message: Message) {
+    await this.consumer.acknowledge(message);
+  }
+
+  protected abstract onMessage(message: Message): Promise<void>;
+}
