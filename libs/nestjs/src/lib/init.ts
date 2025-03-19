67f00077b49e645b69700d71d5bc41befcd5afdf
Nazim <yasarnazimsari@gmail.com>
2025-03-19 22:54:45 +0300

    created new nestjs lib updated old nestjs lib folder  rename graphgl folder

--- libs/nestjs/src/lib/init.ts
+++ libs/nestjs/src/lib/init.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
